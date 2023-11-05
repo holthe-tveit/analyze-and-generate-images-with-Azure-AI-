cf7093b8dbe816c256b4d8c46b894db9dd0882ef
Tomas Eilev Holthe-Tveit <tomas@holthe-tveit.no>
2023-11-05 10:15:22 +0100

    add v0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  //A title for the page
+  //A text box to enter the URL of the image to be analyzed or the prompt of the image to  generate
+  //A button to trigger the image analysis and another button to trigger the image generation
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Image Analyzer and Generator
+        </p>
+        <img src="https://i.imgur.com/3ZwvW7c.jpg" className="App-logo" alt="logo" />
+        <p>
+          Enter the URL of the image to be analyzed or the prompt of the image to be generated
+        </p>
+        <input type="text" placeholder="URL or Prompt" />
+        <button>
+          Analyze
+        </button>
+        <button>
+          Generate
+        </button>
+      </header>
+    </div>
+  );
+  //A title for the page
+  //A text box to enter the URL of the image to be analyzed or the prompt of the image to  generate
+  //A button to trigger the image analysis and another button to trigger the image generation
+
 }
 
 export default App;
